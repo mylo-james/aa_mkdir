0fd460d665aee6124b81530eb8a23b96d411ddcd
Mylo James <mjames@appacademy.io>
2021-07-20 12:15:12 -0500

    added script

---
+++ run.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+mkdir appacademy && 
+cd appacademy && 
+git clone https://github.com/appacademy/Module-1-Resources.git && 
+mkdir \
+W1 \
+W1/D1 \
+W1/D1/lecture W1/D1/projects W1/D1/homework \
+W1/D2 \
+W1/D2/lecture W1/D2/projects W1/D2/homework \
+W1/D3 \
+W1/D3/lecture W1/D3/projects W1/D3/homework \
+W1/D4 \
+W1/D4/lecture W1/D4/projects W1/D4/homework \
+W1/D5 \
+W1/D5/lecture W1/D5/projects W1/D5/homework \
+W2 \
+W2/D1 \
+W2/D1/lecture W2/D1/projects W2/D1/homework \
+W2/D2 \
+W2/D2/lecture W2/D2/projects W2/D2/homework \
+W2/D3 \
+W2/D3/lecture W2/D3/projects W2/D3/homework \
+W2/D4 \
+W2/D4/lecture W2/D4/projects W2/D4/homework \
+W2/D5 \
+W2/D5/lecture W2/D5/projects W2/D5/homework \
+W3 \
+W3/D1 \
+W3/D1/lecture W3/D1/projects W3/D1/homework \
+W3/D2 \
+W3/D2/lecture W3/D2/projects W3/D2/homework \
+W3/D3 \
+W3/D3/lecture W3/D3/projects W3/D3/homework \
+W3/D4 \
+W3/D4/lecture W3/D4/projects W3/D4/homework \
+W3/D5 \
+W3/D5/lecture W3/D5/projects W3/D5/homework \
