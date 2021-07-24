efa8d6c3f2adcf6be50206700c9da8b29ae115da
Lazytangent <pm123633@gmail.com>
2021-07-24 18:49:32 -0500

    Use curly braces for expansion

--- run.sh
+++ run.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 cd ~ &&
-mkdir appacademy && 
-cd appacademy && 
-git clone https://github.com/appacademy/Module-1-Resources.git && 
+mkdir appacademy &&
+cd appacademy &&
+git clone https://github.com/appacademy/Module-1-Resources.git &&
 mkdir -p \
-w1/d1/lecture w1/d1/projects w1/d1/homework \
-w1/d2/lecture w1/d2/projects w1/d2/homework \
-w1/d3/lecture w1/d3/projects w1/d3/homework \
-w1/d4/lecture w1/d4/projects w1/d4/homework \
-w1/d5/lecture w1/d5/projects w1/d5/homework \
-w2/d1/lecture w2/d1/projects w2/d1/homework \
-w2/d2/lecture w2/d2/projects w2/d2/homework \
-w2/d3/lecture w2/d3/projects w2/d3/homework \
-w2/d4/lecture w2/d4/projects w2/d4/homework \
-w2/d5/lecture w2/d5/projects w2/d5/homework \
-w3/d1/lecture w3/d1/projects w3/d1/homework \
-w3/d2/lecture w3/d2/projects w3/d2/homework \
-w3/d3/lecture w3/d3/projects w3/d3/homework \
-w3/d4/lecture w3/d4/projects w3/d4/homework \
-w3/d5/lecture w3/d5/projects w3/d5/homework
+{w1,w2,w3}/{d1,d2,d3,d4,d5}/{lecture,projects,homework}
