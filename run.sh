b891517c14f188ce0b12a8ca964b793097466b0b
Lazytangent <pm123633@gmail.com>
2021-07-24 19:41:40 -0500

    Add conditionals to prevent some possible errors

--- run.sh
+++ run.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-cd ~ &&
-mkdir appacademy &&
-cd appacademy &&
-git clone https://github.com/appacademy/Module-1-Resources.git &&
+cd ~
+mkdir -p appacademy
+cd appacademy
+if [ ! -d Module-1-Resources ]; then
+  git clone https://github.com/appacademy/Module-1-Resources.git
+else
+  cd Module-1-Resources
+  git pull
+  cd ../
+fi
+
 mkdir -p \
 {w1,w2,w3}/{d1,d2,d3,d4,d5}/{lecture,projects,homework}
